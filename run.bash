4daa75b10e9a475df4eaa379e8152d735d397ad0
zhouzt21 <zhouzt6@gmail.com>
2025-06-03 11:41:44 +0800

    update

--- run.bash
+++ run.bash
@@ -6,5 +6,5 @@
 # python script/run.py --config-name=pre_diffusion_mlp \
 #     --config-dir=cfg/gym/pretrain/hopper-medium-v2    
 
-python script/run.py --config-name=ft_ppo_diffusion_mlp \
+python script/run.py --config-name=ft_ppo_diffusion_img \
     --config-dir=cfg/sapien/finetune/pick_and_place_panda
