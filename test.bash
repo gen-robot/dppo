a265e87276b12400ab68936bd7d3436c8ab2499d
zhouzt21 <zhouzt6@gmail.com>
2025-06-05 16:09:54 +0800

    task

--- test.bash
+++ test.bash
@@ -1,19 +1,23 @@
+# 8107
 python script/run.py --config-name=ft_ppo_diffusion_img \
     --config-dir=cfg/sapien/finetune/open_door 
 
-python script/run.py --config-name=ft_ppo_diffusion_img \
-    --config-dir=cfg/sapien/finetune/drawer
-
+# 8107
 python script/run.py --config-name=ft_ppo_diffusion_img \
     --config-dir=cfg/sapien/finetune/microwave
 
+# sichang
+python script/run.py --config-name=ft_ppo_diffusion_img \
+    --config-dir=cfg/sapien/finetune/drawer
+# sichang
 python script/run.py --config-name=ft_ppo_diffusion_img \
     --config-dir=cfg/sapien/finetune/pick_and_place
 
+# 8105
 python script/run.py --config-name=ft_ppo_diffusion_img \
     --config-dir=cfg/sapien/finetune/open_door ft_denoising_steps=10
 
-
+# 8105
 python script/run.py --config-name=ft_ppo_diffusion_img \
     --config-dir=cfg/sapien/finetune/drawer ft_denoising_steps=10
 
